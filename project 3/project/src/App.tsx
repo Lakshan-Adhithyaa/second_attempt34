a042d6bb94ed9dd846e5fa4dd9c5fb431cdcd7f7
Lakshan-Adhithyaa <slakshanadithyaa@gmail.com>
2025-02-07 22:01:58 +0530

    Update App.tsx

--- project 3/project/src/App.tsx
+++ project 3/project/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import { PrivateRoute } from './components/PrivateRoute';
 import Splash from './pages/Splash';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
@@ -10,25 +12,42 @@ import Profile from './pages/Profile';
 import AICoach from './pages/AICoach';
 
 function App() {
-  const shouldShowNavbar = (pathname: string) => {
-    const noNavbarRoutes = ['/', '/login', '/signup', '/user-info'];
-    return !noNavbarRoutes.includes(pathname);
-  };
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Splash />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/user-info" element={<UserInfo />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/categories" element={<WorkoutCategories />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/ai-chat" element={<AICoach />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Splash />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/user-info" element={
+            <PrivateRoute>
+              <UserInfo />
+            </PrivateRoute>
+          } />
+          <Route path="/dashboard" element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          } />
+          <Route path="/categories" element={
+            <PrivateRoute>
+              <WorkoutCategories />
+            </PrivateRoute>
+          } />
+          <Route path="/profile" element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          } />
+          <Route path="/ai-chat" element={
+            <PrivateRoute>
+              <AICoach />
+            </PrivateRoute>
+          } />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
